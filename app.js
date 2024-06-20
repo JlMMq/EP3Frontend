16b75240edae463f0850721060ecdf0afdc793cb
JlMMq <117398060+JlMMq@users.noreply.github.com>
2024-06-19 22:34:27 -0500

    R

--- app.js
+++ app.js
@@ -11,23 +11,6 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
 })
 
-/*
-app.post('/solicitud',(req,res) => {
-    const solicitud = {
-        nombre: `${req.body.name}`,
-        correo: `${req.body.email}`,
-        asunto: `${req.body.subject}`,
-        mensaje: `${req.body.message}`
-    }
-    console.log('Enviado por: ' + solicitud.nombre +'\n'
-        + 'Con correo: ' + solicitud.correo + '\n'
-        + 'Asunto: ' + solicitud.asunto + '\n'
-        + 'Mensaje: ' + solicitud.mensaje  
-    )
-    res.sendFile(__dirname + '/public/contact.html')
-})
-*/
-
 app.use(express.static('public'))
 
 app.listen(config.port, () => {
